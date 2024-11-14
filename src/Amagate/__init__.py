e5c319d22630a075ccc1282fd46359c966b40f43
Sryml <sryml@hotmail.com>
2024-11-14 22:14:53 +0800

    🧪 test: 更改插件重载逻辑

--- src/Amagate/__init__.py
+++ src/Amagate/__init__.py
@@ -1,22 +1,34 @@
 import importlib
 
+import bpy
+from . import data, operator, panel, translations  # 导入插件其他模块
 
-from . import operator, panel, data, translations  # 导入插件其他模块
+loaded = False
 
 
 # 注册和取消注册函数
-def register(reload=False):
-    if reload:
-        importlib.reload(data)
-        importlib.reload(operator)
-        importlib.reload(panel)
-        importlib.reload(translations)
+def register():
+    global loaded
+    if loaded:
+        import sys
+
+        for module in ("data", "operator", "panel", "translations"):
+            del sys.modules[f"{__package__}.{module}"]
+        # from . import data, operator, panel, translations
+        for module in ("data", "operator", "panel", "translations"):
+            globals()[module] = importlib.import_module(f"{__package__}.{module}")
+        # importlib.reload(data)
+        # importlib.reload(operator)
+        # importlib.reload(panel)
+        # importlib.reload(translations)
         print("Amagate reload")
 
     data.register()
     operator.register()
     panel.register()
     translations.register()
+
+    loaded = True
     print("Amagate register")
 
 
