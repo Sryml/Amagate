ba972bc39eab402c05fe87bb1cb266556dd518db
Sryml <sryml@hotmail.com>
2024-11-26 23:43:17 +0800

    🌈 style: 扩展类型提示

---
+++ src/Amagate/bpy_stub.pyi
@@ -0,0 +1,18 @@
+from . import data as ag_data
+
+import bpy
+from bpy import *
+
+class __Image(bpy.types.Image):
+    amagate_data: ag_data.ImageProperty
+
+class __Object(bpy.types.Object):
+    amagate_data: ag_data.ObjectProperty
+
+class __Scene(bpy.types.Scene):
+    amagate_data: ag_data.SceneProperty
+
+class __Context(bpy.types.Context):
+    scene: __Scene
+
+context: __Context
