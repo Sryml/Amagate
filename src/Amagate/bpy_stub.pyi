8c6051009135fb922beaf278c1ed4cf90180563c
Sryml <sryml@hotmail.com>
2025-04-27 01:08:28 +0800

    📃 docs: 更新模块存根

--- src/Amagate/bpy_stub.pyi
+++ src/Amagate/bpy_stub.pyi
@@ -1,7 +1,7 @@
 from .scripts import data as ag_data
 
 import bpy
-from bpy import *
+from bpy import *  # type: ignore
 
 class __Image(bpy.types.Image):
     amagate_data: ag_data.ImageProperty
@@ -14,5 +14,22 @@ class __Scene(bpy.types.Scene):
 
 class __Context(bpy.types.Context):
     scene: __Scene
+    selected_objects: list[__Object]
+    active_object: __Object
+
+class __BlendDataImages(bpy.types.BlendDataImages):
+    def __getitem__(self, key: str | int | slice) -> __Image | list[__Image]: ...
+
+class __BlendDataObjects(bpy.types.BlendDataObjects):
+    def __getitem__(self, key: str | int | slice) -> __Object | list[__Object]: ...
+
+class __BlendDataScenes(bpy.types.BlendDataScenes):
+    def __getitem__(self, key: str | int | slice) -> __Scene | list[__Scene]: ...
+
+class __BlendData(bpy.types.BlendData):
+    images: __BlendDataImages
+    objects: __BlendDataObjects
+    scenes: __BlendDataScenes
 
 context: __Context
+data: __BlendData
